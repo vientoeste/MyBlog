cf980e7329a759d46eb5a60ec811041e99290544
este <djjd2000@naver.com>
2023-04-01 05:28:43 +0900

    Chore: updated eslint arrow-body-style rule to force one-line arrow functions without braces

--- lib/util.ts
+++ lib/util.ts
@@ -1,3 +1,1 @@
-export const getDateForDb = () => {
-  return new Date().toISOString().replace(/T|Z/g, ' ').slice(0, -5);
-};
+export const getDateForDb = () => new Date().toISOString().replace(/T|Z/g, ' ').slice(0, -5);
