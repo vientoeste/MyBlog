f8cd0ac7b96ddbacf4ff7033428307e2ecfda957
este <djjd2000@naver.com>
2023-04-07 19:46:27 +0900

    Feature: added cache and single post component to Post component

--- client/src/Posts.js
+++ client/src/Posts.js
@@ -3,12 +3,49 @@ import axios from 'axios';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [cachedPosts, setCachedPosts] = useState([]);
 
+  // [TODO] 전체 포스트 조회 시와 단일 포스트 조회 시 같은 타입으로 전송하기 때문에 통일된 캐싱 사용
+  // 포스트 미리보기 기능 추가 혹은 실제 포스트 작성 시작 시 캐시를 나눠야 함
   useEffect(() => {
-    axios.get('/api-proxy/posts')
-      .then(response => setPosts(response.data))
-      .catch(error => console.error(error));
-  }, []);
+    if (cachedPosts.length > 0) {
+      setPosts(cachedPosts);
+    } else {
+      axios.get('/api-proxy/posts')
+        .then(response => {
+          setPosts(response.data);
+          setCachedPosts(response.data);
+        })
+        .catch(error => console.error(error));
+    }
+  }, [cachedPosts]);
+
+  function handlePostClick(postUuid) {
+    const cachedPost = cachedPosts.find(post => post.uuid === postUuid);
+
+    if (cachedPost) {
+      setPosts([cachedPost]);
+    } else {
+      axios.get(`/api-proxy/posts/${postUuid}`)
+        .then(response => {
+          setPosts([response.data]);
+          setCachedPosts([...cachedPosts, response.data]);
+        })
+        .catch(error => console.error(error));
+    }
+  }
+
+  if (posts.length === 1) {
+    const currentPost = posts[0];
+
+    return (
+      <div className="post">
+        <h2>{currentPost.title}</h2>
+        <p>{currentPost.content}</p>
+        <p>{currentPost.categoryId}</p>
+      </div>
+    );
+  }
 
   return (
     <div className="posts">
@@ -16,11 +53,11 @@ function Posts() {
       <ul>
         {posts.map(post => (
           <li key={post.uuid}>
-            <a href={`/posts/${post.uuid}`}>
+            <button onClick={() => handlePostClick(post.uuid)}>
               <h3>{post.title}</h3>
               <p>{post.content}</p>
-              <p>{post.category_id}</p>
-            </a>
+              <p>{post.categoryId}</p>
+            </button>
           </li>
         ))}
       </ul>
