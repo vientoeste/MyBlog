60652e455c0491c502c66f75fa873c95310a16f0
este <djjd2000@naver.com>
2023-05-06 23:53:02 +0900

    Refac: added PKTypes

--- server/interfaces/Entity.ts
+++ server/interfaces/Entity.ts
@@ -1,16 +1,18 @@
-export interface PostEntity {
+export interface PKTypes {
+  id: number;
   uuid: string;
+}
+
+export interface PostEntity extends Pick<PKTypes, 'uuid'> {
   title: string;
   content: string;
   category_id: number;
 }
-export interface CategoryEntity {
-  id: number;
+export interface CategoryEntity extends Pick<PKTypes, 'id'> {
   name: string;
 }
 
-export interface CommentEntity {
-  uuid: string;
+export interface CommentEntity extends Pick<PKTypes, 'uuid'> {
   post_uuid: string;
   user_id: string;
   content: string;
