a8135be3bc5fa5f767b7037aad919f50af3d0e60
este <djjd2000@naver.com>
2023-05-09 23:21:40 +0900

    Fix: separated clean-dist

--- .scripts/clean-dist.js
+++ .scripts/clean-dist.js
@@ -1,13 +1,22 @@
 const { rmSync } = require("fs");
 const { resolve } = require("path");
 
-const serverDistFolder = resolve(__dirname, "..", "server", "dist");
-const proxyDistFolder = resolve(__dirname, "..", "proxy", "dist");
-
-try {
-  rmSync(proxyDistFolder, { recursive: true });
-  rmSync(serverDistFolder, { recursive: true });
-} catch (err) {
-  console.error(`Error while deleting ${proxyDistFolder}.`);
-  console.error(`Error while deleting ${serverDistFolder}.`);
+switch (process.argv[2]) {
+  case 'server':
+    const serverDistFolder = resolve(__dirname, "..", "server", "dist");
+    try {
+      rmSync(serverDistFolder, { recursive: true });
+    } catch (err) {
+      console.error(`Error while deleting ${serverDistFolder}.`);
+    }
+    break;
+  case 'proxy':
+    const proxyDistFolder = resolve(__dirname, "..", "proxy", "dist");
+    try {
+      rmSync(proxyDistFolder, { recursive: true });
+    } catch (err) {
+      console.error(`Error while deleting ${proxyDistFolder}.`);
+    }
+  default:
+    break;
 }
